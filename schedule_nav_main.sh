ac525dde129567667d079c1d7118ddb58409f1e8
jakiroshah <aalap.shah@yale.edu>
2026-07-05 17:44:04 -0400

    running on misha

--- schedule_nav_main.sh
+++ schedule_nav_main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=genea_blender
+#SBATCH --job-name=genea_main
 #SBATCH --partition=day
 #SBATCH --cpus-per-task=42
-#SBATCH --mem=16G
+#SBATCH --mem=64G
 #SBATCH --mail-user=aalap.shah@yale.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=job_%A_%a.out
